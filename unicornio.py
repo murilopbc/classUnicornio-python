f129b47ace327f82f216c304f2e561db5d590306
murilopbc <murilopoltronieri3@gmail.com>
2023-11-14 11:13:35 -0300

    unicornio

--- unicornio.py
+++ unicornio.py
@@ -1,78 +1,94 @@
-import random
-#############################################   IMPORTS   ##############################################################
+# IMPORTAR A FUNÇÃO RANDINT
+
+from random import randint
 
 
+# CRIAÇÃO DA CLASSE UNICÓRNIO COM 5 ATRIBUTOS, SENDO 2 PRIVADOS
 
 
-############################################# CLASSE UNICORNIO  ########################################################
 class Unicornio:
-    def __init__(self,nome, cor_rabo, tamanho, cor_corpo, portador, alimentação):
-        self.nome = nome
-        self.cor_rabo = cor_rabo
-        self.tamanho = tamanho
-        self.cor = cor_corpo
-        self.portador = portador
-        self.alimentacao = alimentação
+    # DECLARAÇÃO DO PORTE PADRÃO DO UNICÓRNIO
 
+    porte = "pequeno"
 
-    def menudejogo (self):
-      input("Escolha uma opção: ")
+    def __init__(self, nome: str, cor: str, chifre: int, peso: float, velocidade: float):
+        self.__nome = nome
+        self.cor = cor
+        self.chifre = chifre
+        self.__peso = peso
+        self.velocidade = velocidade
 
-    def pergunta1 (self):
-        userdois = input(f"Voce acabou de adotar um unicornio chamado {nome}.agora vamos alimenta-lo.aperte dar comida para alimenta-lo  ").lower()
-        if userdois == "Dar comida":
-              print(f"{portador} e {nome} foram voar perto da montanha bosch")
+    # CRIAÇÃO DO MÉTODO ESTÁTICO PARA SORTEAR A IDADE DO UNICÓRNIO
+    @staticmethod
+    def gerarIdade():
+        rand = randint(1, 100)
+        return f"Ele(a) tem {rand} anos"
+
+    # CRIAÇÃO DO ATRIBUTO ESTÁTICO PARA EXIBIR O PORTE PADRÃO DO UNICÓRNIO
+    @staticmethod
+    def obter_porte():
+        return f"Seu unicórnio é de porte {Unicornio.porte}"
+
+    # FUNÇÃO DO GET(ACESSAR) E SETTER(MODIFICAR) PARA OS ATRIBUTOS PRIVADOS
+    def getNome(self):
+        return self.__nome
+
+    def setNome(self, nome):
+        self.__nome = nome
+
+    def getPeso(self):
+        return self.__peso
+
+    def setPeso(self, peso):
+        self.__peso = peso
+
+    # CRIAÇÃO DO PROPERTY
+    @property
+    def nome(self):
+        return self.__nome
+
+    # CRIAÇÃO DO PROPERTY SETTER
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    # FUNÇÃO CRIAR NOME DO UNICÓRNIO
+    def criar_nome(self):
+        nome = input("Digite o nome do seu unicórnio: ")
+        self.__nome = nome
+
+    # FUNÇÃO VOAR
+    def voar(self):
+        peso = float(input("Digite o peso do unicórnio: "))
+        self.__peso = peso
+        velocidade = self.getPeso() * 0.5
+        self.velocidade = velocidade
+        print(f"{self.getNome()}, você pode voar até {self.velocidade} km/h!")
+        if self.velocidade >= 100:
+            print("Impressionante, você é muito veloz!")
+        elif self.velocidade >= 50:
+            print("Você é capaz de atingir uma velocidade considerável no céu!")
         else:
-            print(f"nenhuma opção foi selecio")
-            return
+            print("Infelizmente você enfrenta dificuldades ao voar longas distâncias!")
 
-    print("=" * 20)
-
-    def pergunta2 (self):
-        usertres = input(f"No caminho pra casa {nome} quer voar em alta velocidade,você aceita? ").lower()
-        if usertres == "Não":
-           print("Vocês dois chegaram salvos em casa")
+    # FUNÇÃO POUSAR
+    def pousar(self):
+        escolha = input(f"{self.getNome()}, você deseja pousar: ").lower()
+        if escolha == "sim":
+            print("Muito bem, vamos pousar!")
         else:
-            print("Vocês dois bateram na caixa d'agua e morreram...")
-            exit()
+            print(f"Ok {self.getNome()}, continue aproveitando seu voo!")
 
 
-
-################################################# PRINTS ###############################################################
-
-print("FABRICA DE UNICORNIO D JÃ1")
-print("~-" *13)
-print("=" * 20)
-nome = input("Qual vai ser o nome dele(a)? ")
-print("=" * 20)
-cor_corpo = input("Qual irá ser a cor do corpo? ")
-print("=" * 20)
-cor_rabo = input("Qual vai ser a cor do rabo? ")
-print("=" * 20)
-alimentacao = input("O que ele(a) vai comer? ")
-print("=" * 20)
-tamanho = input("Qual vai ser o tamanho dele(a)? ")
-print("=" * 20)
-portador = input("Quem irá montar nele(a)? ")
-print("=" * 20)
-print(f"Parabens voce criou seu primeiro unicornio! agora vamos ver qual irá ser sua personalidade")
-personalidades = ['Alegre','Revoltado','Bad boy','Festeiro','Crossfiteiro','inteligente','Carente','Engraçado','Preguiçoso','ranzinza']
-print("=" * 20)
-print("Personalidade: " + personalidades [random.randrange(len(personalidades))])
-print("=" * 20)
+# CRIAÇÃO DO OBJETO
 
 
-print("BEM VINDO AO REINO MAGICO DOS UNICORNIOS")
-print("1-Começar historia")
-print("2-Fechar jogo")
+unicornio = Unicornio("", "", "", "", "")
 
-# PARA PRINTAR
+# CHAMAR AS FUNÇÕES
 
-unicornio = Unicornio(nome, cor_rabo, tamanho, cor_corpo, portador, alimentacao)
-
-unicornio.menudejogo()
-
-
-unicornio.pergunta1()
-
-unicornio.pergunta2()
+unicornio.criar_nome()
+print(Unicornio.obter_porte())  # CHAMAR FUNÇÃO DO ATRIBUTO ESTÁTICO
+print(Unicornio.gerarIdade())  # CHAMAR FUNÇÃO DO MÉTODO ESTÁTICO
+unicornio.voar()
+unicornio.pousar()
